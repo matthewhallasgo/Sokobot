77f55dfb48a4575bdbdd67f0326bbead623e6584
juanmiguelrivera-beep <juan_miguel_rivera@dlsu.edu.ph>
2026-06-26 18:30:29 +0800

    Adjust hCost weight in fCost calculation

--- LatestOptimized(resent).java
+++ LatestOptimized(resent).java
@@ -340,7 +340,7 @@ class State {
     }
 
     public int fCost() {
-        return gCost + 5 * hCost; // added weight 10 to hCost
+        return gCost + 5 * hCost; // added weight 5 to hCost
     }
 
     public String reconstructPath() {
